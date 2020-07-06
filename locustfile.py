3a5045a67593f9cd78ffbe36bdf7fad6cd5e8b51
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2020-07-06 20:32:21 +0600

    new added locust

--- locustfile.py
+++ locustfile.py
@@ -1,41 +1,15 @@
-from locust import HttpLocust, TaskSet, task 
-from locust import ResponseError 
-import json 
+import random
+from locust import HttpUser, task, between
 
-
-class UserBehavior(TaskSet): 
-
-	def __init__(self, parent): 
-		super(UserBehavior, self).__init__(parent) 
-		self.token = "" 
-		self.headers = {} 
-
-	def on_start(self): 
-		# The on_start method is called 
-		# when a simulated user starts 
-		# executing that TaskSet class 
-		self.token = self.login() 
-		self.headers = {'Authorization': 'Bearer {}'.format(self.token)} 
-		self.login() 
-
-	def login(self): 
-		# admin login and retrieving it's access token 
-		response = self.client.post("/login/", 
-									data = {'username': 'admin', 
-										'password': 'ZYT5nsg3565!'}) 
-		
-		return json.loads(response._content)['access'] 
-        
+class MyUser(HttpUser):
     @task
-    def get_something(self):
-        self.client.get("/something")    
+    def index(self):
+        self.client.get("/")
+        self.client.get("/web")
+    
+    
+    
+    def on_start(self):
+        self.client.post("/login", {"username":"foo", "password":"bar"})
 
-
-
-class WebsiteUser(HttpLocust): 
-	# The task_set attribute should point 
-	# to a TaskSet class which defines 
-	# the behaviour of the user 
-	task_set = UserBehavior 
-	min_wait = 5000
-	max_wait = 9000
+    wait_time = between(5, 9)
